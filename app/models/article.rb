26a06fb18f3c7fc9cc1d7b32a50e2ec8ba7f97be
Thanthou <Thanthou@Brandans-MacBook-Pro.local>
2016-05-24 16:51:23 -0700

    fixed tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,7 @@ class Article < ActiveRecord::Base
     has_many :tags, through: :taggings
     has_attached_file :image
     validates_attachment_content_type :image, :content_type => ["image/jpb","image/jpeg", "image/png"]
-    def tags
-      @tag
-    end
+    
 
     def tag_list
       self.tags.collect do |tag|
--- db/migrate/20160524225720_create_taggings.rb
+++ db/migrate/20160524225720_create_taggings.rb
@@ -2,8 +2,7 @@ class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
       t.references :tag, index: true, foreign_key: true
-      t.string :article
-      t.string :references
+      t.references :article, index: true, foreign_key: true
 
       t.timestamps null: false
     end
